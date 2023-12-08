4ebb28b937162a305b861026b04755d3d7318bdb
Curtis Rueden <ctrueden@wisc.edu>
2023-12-08 13:44:51 -0600

    Add working native code to launch Java main method
    The next step is to wrap the jaunch native library to KMP...

---
+++ jaunch/jaunch.h
@@ -0,0 +1,7 @@
+#ifndef JAUNCH_H
+#define JAUNCH_H
+
+extern int launch_java(const char *libjvm_path, const int jvm_argc, const char *jvm_argv[],
+	const char *main_class_name, const int main_argc, const char *main_argv[])
+
+#endif
