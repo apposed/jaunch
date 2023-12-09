0b16e634045b06c18d7eb0b0ac4e0fb35d7da404
Curtis Rueden <ctrueden@wisc.edu>
2023-12-08 22:09:13 -0600

    Tweak -l flags

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-gcc -shared -Wl,--no-as-needed -I/usr/lib/jvm/default-java/include -I/usr/lib/jvm/default-java/include/linux -fPIC -fno-stack-protector jaunch/jaunch.c -o libjaunch.so
-gcc -I./jaunch -L. -ljaunch -fPIC -fno-stack-protector jaunch/fiji.c -o launch-fiji
+gcc -shared -Wl,--no-as-needed -I/usr/lib/jvm/default-java/include -I/usr/lib/jvm/default-java/include/linux -fPIC -fno-stack-protector jaunch/jaunch.c -o libjaunch.so -ldl
+gcc -I./jaunch -L. -fPIC -fno-stack-protector jaunch/fiji.c -o launch-fiji -ljaunch
