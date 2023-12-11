57de47e907fdbc595303c97e120c23200dfa83b4
Curtis Rueden <ctrueden@wisc.edu>
2023-12-11 16:16:54 -0600

    Second version of executable runner
    With line splitting.

--- jaunch/runthing.c
+++ jaunch/runthing.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main() {
-    // Command to execute (replace "ls -l" with your desired command)
-    const char *command = "ls -l";
-
+// Function to run a command and capture its stdout lines
+char **runCommand(const char *command, size_t *numLines) {
     // Open a pipe to the process
     FILE *pipeStream = popen(command, "r");
     if (pipeStream == NULL) {
@@ -14,12 +13,23 @@ int main() {
 
     // Buffer to store the output
     char buffer[1024];
-    size_t bytesRead;
+    size_t totalSize = 0;
 
-    // Read the output from the process
+    // Read the entire output into a single string
+    char *outputString = NULL;
+    size_t bytesRead;
     while ((bytesRead = fread(buffer, 1, sizeof(buffer), pipeStream)) > 0) {
-        fwrite(buffer, 1, bytesRead, stdout);  // Display the output
+        outputString = realloc(outputString, totalSize + bytesRead + 1);
+        if (outputString == NULL) {
+            perror("realloc");
+            exit(EXIT_FAILURE);
+        }
+        memcpy(outputString + totalSize, buffer, bytesRead);
+        totalSize += bytesRead;
     }
+
+    // Null-terminate the output string
+    outputString[totalSize] = '\0';
 
     // Close the pipe
     int status = pclose(pipeStream);
@@ -34,6 +44,54 @@ int main() {
             printf("Child process did not exit normally\n");
         }
     }
+
+    // Count the number of lines in the output
+    size_t lineCount = 0;
+    for (size_t i = 0; i < totalSize; i++) {
+        if (outputString[i] == '\n') {
+            lineCount++;
+        }
+    }
+
+    // Allocate an array of strings to store the lines
+    char **lines = (char **)malloc(lineCount * sizeof(char *));
+    if (lines == NULL) {
+        perror("malloc");
+        exit(EXIT_FAILURE);
+    }
+
+    // Split the output string into lines
+    size_t lineIndex = 0;
+    char *token = strtok(outputString, "\n");
+    while (token != NULL) {
+        lines[lineIndex] = strdup(token);
+        if (lines[lineIndex] == NULL) {
+            perror("strdup");
+            exit(EXIT_FAILURE);
+        }
+        lineIndex++;
+        token = strtok(NULL, "\n");
+    }
+
+    *numLines = lineCount;
+    free(outputString); // Free the temporary string
+    return lines;
+}
+
+int main() {
+    const char *command = "ls -l";
+
+    size_t numLines;
+    char **outputLines = runCommand(command, &numLines);
+
+    // Display the output lines
+    for (size_t i = 0; i < numLines; i++) {
+        printf("Line %zu: %s\n", i + 1, outputLines[i]);
+        free(outputLines[i]); // Free each string
+    }
+
+    // Free the array of lines
+    free(outputLines);
 
     return 0;
 }
