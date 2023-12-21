2c823050e9b3bae6565ea16e5055fd0b9531a280
Curtis Rueden <ctrueden@wisc.edu>
2023-12-20 22:36:07 -0600

    Fix logging

--- src/commonMain/kotlin/logging.kt
+++ src/commonMain/kotlin/logging.kt
@@ -1,6 +1,6 @@
 private val DEBUG = getenv("DEBUG") !in listOf(null, "", "0", "false", "FALSE")
 
-fun debug(vararg args: Any) { if (DEBUG) report("DEBUG", args) }
+fun debug(vararg args: Any) { if (DEBUG) report("DEBUG", *args) }
 
 fun debugList(message: String, items: Collection<String>) {
     debug()
@@ -9,7 +9,7 @@ fun debugList(message: String, items: Collection<String>) {
     items.forEach { debug("* $it") }
 }
 
-fun warn(vararg args: Any) { report("WARNING", args) }
+fun warn(vararg args: Any) { report("WARNING", *args) }
 
 private fun report(prefix: String, vararg args: Any) {
     printlnErr(buildString {
