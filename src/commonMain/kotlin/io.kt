b4b664b146694f672766f12ab8fc080e24ec1081
Curtis Rueden <ctrueden@wisc.edu>
2023-12-31 20:37:44 -0600

    Add debug line when reading a config file

--- src/commonMain/kotlin/io.kt
+++ src/commonMain/kotlin/io.kt
@@ -3,6 +3,7 @@ import com.akuleshov7.ktoml.file.TomlFileReader
 import kotlinx.serialization.serializer
 
 fun readConfig(tomlPath: String): JaunchConfig {
+    debug("Reading config file: $tomlPath");
     val tomlFile = File(tomlPath)
     if (!tomlFile.exists) return JaunchConfig()
     return TomlFileReader(
