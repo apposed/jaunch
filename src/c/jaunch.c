57bdac6487d6c07c7876e39d9812974ddaeafbdd
Curtis Rueden <ctrueden@wisc.edu>
2023-12-31 20:38:27 -0600

    Get run_command closer to working on Windows
    I tried to use _popen instead of the Windows API (CreatePipe,
    CreateProcess, ReadFile, WriteFile), but couldn't get it working.
    It would always hang after writing to the subprocess's stdin,
    even if I was careful to close the stdin pipe afterward.
    
    This version has a lot of code duplicated from the posix version,
    which can hopefully be consolidated with some refactoring passes.
    
    Here is the failed _popen version:
    
        debug("run_command: opening pipe to/from jaunch");
        FILE *pipe = _popen(command, "r+");
        if (pipe == NULL) {
            perror("Error opening pipe");
            return 1;
        }
    
        // Write to the child process's stdin
        debug("run_command: writing to jaunch stdin");
        // Passing the input line count as the first line tells the child process what
        // to expect, so that it can stop reading from stdin once it has received
        // those lines, even though the pipe is not yet closed. This avoids deadlocks.
        fprintf(pipe, "%d\n", numInput);
        fflush(pipe);
        debug("run_command: wrote numInput: %d", numInput);
        for (size_t i = 0; i < numInput; i++) {
            fprintf(pipe, "%s\n", input[i]);
            debug("run_command: wrote input #%d: %s", i, input[i]);
        }
        fflush(pipe);
    
        // Read data from stdout
        debug("run_command: reading from jaunch stdout");
        char buffer[65536];
        size_t lineCount = 0;
        *output = NULL;
        while (fgets(buffer, sizeof(buffer), pipe) != NULL) {
            debug("run_command: got %d bytes from jaunch", strlen(buffer));
            *output = realloc(*output, (lineCount + 1) * sizeof(char *));
            (*output)[lineCount] = strdup(buffer);
            if ((*output)[lineCount] == NULL) {
                error("strdup");
                return ERROR_STRDUP;
            }
            lineCount++;
        }
        *numOutput = lineCount;
    
        // Close the pipe
        if (_pclose(pipe) == -1) {
            perror("Error closing pipe");
            return 1;
        }
    
        return 0;

--- src/c/jaunch.c
+++ src/c/jaunch.c
@@ -107,15 +107,36 @@ void handleError(const char* errorMessage) {
 	exit(1);
 }
 
+void writeLine(HANDLE stdinWrite, const char *input) {
+    DWORD bytesWritten;
+
+    // Calculate the length of the input string
+    size_t inputLength = strlen(input);
+
+    // Allocate memory for the string with newline
+    char *line = (char *)malloc(inputLength + 2);  // +1 for newline, +1 for null terminator
+
+    // Copy the input string and add a newline
+    strcpy(line, input);
+    strcat(line, "\n");
+
+    // Write the string with newline to the pipe
+    if (!WriteFile(stdinWrite, line, inputLength + 1, &bytesWritten, NULL))
+			handleError("Error writing to stdin");
+
+    // Free allocated memory
+    free(line);
+}
+
 int run_command(const char *command,
 	const char *input[], size_t numInput,
 	char ***output, size_t *numOutput)
 {
-	// Thanks to ChatGPT for coding up this routine.
-
 	// Create pipes for stdin and stdout
 	HANDLE stdinRead, stdinWrite, stdoutRead, stdoutWrite;
 	SECURITY_ATTRIBUTES sa = { sizeof(SECURITY_ATTRIBUTES), NULL, TRUE };
+
+	debug("run_command: opening pipes to/from jaunch");
 	if (!CreatePipe(&stdinRead, &stdinWrite, &sa, 0) ||
 		!CreatePipe(&stdoutRead, &stdoutWrite, &sa, 0))
 	{
@@ -141,26 +162,46 @@ int run_command(const char *command,
 	CloseHandle(stdinRead);
 	CloseHandle(stdoutWrite);
 
-	// Write data to stdin
-	DWORD bytesWritten;
-	for (size_t i = 0; i < numInput; i++) {
-		if (!WriteFile(stdinWrite, input[i], strlen(input[i]), &bytesWritten, NULL)) {
-			handleError("Error writing to stdin");
-		}
-		if (!WriteFile(stdinWrite, "\r\n", 2, &bytesWritten, NULL)) {
-			handleError("Error writing to stdin");
-		}
-	}
+	// Write to the child process's stdin
+	debug("run_command: writing to jaunch stdin");
+	// Passing the input line count as the first line tells the child process what
+	// to expect, so that it can stop reading from stdin once it has received
+	// those lines, even though the pipe is not yet closed. This avoids deadlocks.
+	char *numInputString = (char *)malloc(21);
+	if (numInputString == NULL) error("malloc");
+	snprintf(numInputString, 21, "%zu", numInput);
+	writeLine(stdinWrite, numInputString);
+	free(numInputString);
+	for (size_t i = 0; i < numInput; i++)
+		writeLine(stdinWrite, input[i]);
 
 	// Close the stdin write handle to signal end of input
 	CloseHandle(stdinWrite);
 
-	// Read data from stdout
-	char buffer[65536];
+	// Read from the child process's stdout
+	char buffer[1024];
 	DWORD bytesRead;
+	DWORD totalBytesRead = 0;
+	size_t bufferSize = 1024;
+	char *outputBuffer = malloc(bufferSize);
+
+	if (outputBuffer == NULL) {
+		error("malloc");
+		return ERROR_MALLOC;
+	}
+
 	while (ReadFile(stdoutRead, buffer, sizeof(buffer), &bytesRead, NULL) && bytesRead > 0) {
-		// Process the output data (e.g., print it)
-		fwrite(buffer, 1, bytesRead, stdout);
+		debug("run_command: got %d bytes from jaunch", strlen(buffer));
+		if (totalBytesRead + bytesRead > bufferSize) {
+			bufferSize *= 2;
+			outputBuffer = realloc(outputBuffer, bufferSize);
+			if (outputBuffer == NULL) {
+				error("realloc");
+				return ERROR_REALLOC;
+			}
+		}
+		memcpy(outputBuffer + totalBytesRead, buffer, bytesRead);
+		totalBytesRead += bytesRead;
 	}
 
 	// Close handles
@@ -168,15 +209,38 @@ int run_command(const char *command,
 	CloseHandle(pi.hProcess);
 	CloseHandle(pi.hThread);
 
-	return 0;
+	// Return the output buffer and the number of lines
+	*output = NULL;
+	*numOutput = 0;
+
+	if (totalBytesRead > 0) {
+		// Split the output buffer into lines
+		size_t lineCount = 0;
+		char *token = strtok(outputBuffer, "\n");
+		while (token != NULL) {
+			*output = realloc(*output, (lineCount + 1) * sizeof(char *));
+			if (*output == NULL) {
+				error("realloc");
+				return ERROR_REALLOC2;
+			}
+			(*output)[lineCount] = strdup(token);
+			if ((*output)[lineCount] == NULL) {
+				error("strdup");
+				return ERROR_STRDUP;
+			}
+			lineCount++;
+			token = strtok(NULL, "\n");
+		}
+
+		*numOutput = lineCount;
+		free(outputBuffer); // Free the temporary buffer
+	}
 }
 #else
 int run_command(const char *command,
 	const char *input[], size_t numInput,
 	char ***output, size_t *numOutput)
 {
-	// Thanks to ChatGPT for coding up this routine.
-
 	// Create pipes for stdin and stdout
 	int stdinPipe[2];
 	int stdoutPipe[2];
