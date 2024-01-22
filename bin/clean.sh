7cf1e8d87c7b512b017348c3ace814b7dda6f478
Curtis Rueden <ctrueden@wisc.edu>
2024-01-21 22:27:31 -0600

    Make the clean script nuke macOS's Contents dir

--- bin/clean.sh
+++ bin/clean.sh
@@ -4,4 +4,4 @@ cd "$(dirname "$0")/.."
 echo
 echo -e "\033[1;33m[clean]\033[0m"
 
-rm -rfv build app/jy app/jaunch/jaunch* app/jaunch/*.class
+rm -rfv build app/Contents app/jy app/jaunch/jaunch* app/jaunch/*.class
