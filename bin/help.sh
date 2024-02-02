9fc1c165e025765b395d50ce3b51907e1bf36b8a
Curtis Rueden <ctrueden@wisc.edu>
2024-02-02 14:06:58 -0600

    Fix help targets
    The compile-config target was renamed compile-configurator.

--- bin/help.sh
+++ bin/help.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 echo "Available targets:
-	clean            - remove build files and directories
-	compile-launcher - compile the native launcher (C)
-	compile-config   - compile the Jaunch configurator (Kotlin)
-	compile-all      - compile native launcher and Jaunch configurator
-	app              - generate example application
-	test             - run automated test suite
+	clean                - remove build files and directories
+	compile-launcher     - compile the native launcher (C)
+	compile-configurator - compile the Jaunch configurator (Kotlin)
+	compile-all          - compile native launcher and Jaunch configurator
+	app                  - generate example application
+	test                 - run automated test suite
 "
