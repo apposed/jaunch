68b2b7c3656bb6dd37b95135655af3aec2d287f2
Curtis Rueden <ctrueden@wisc.edu>
2024-02-08 15:47:49 -0600

    bin/clean.sh: remove jaunch.tar.gz bundle, too

--- bin/clean.sh
+++ bin/clean.sh
@@ -4,4 +4,12 @@ cd "$(dirname "$0")/.."
 echo
 echo -e "\033[1;33m[clean]\033[0m"
 
-rm -rfv build dist app/Contents app/jy* app/parsy* app/jaunch/jaunch* app/jaunch/*.class
+rm -rfv \
+  build \
+  dist \
+  app/Contents \
+  app/jy* \
+  app/parsy* \
+  app/jaunch/jaunch* \
+  app/jaunch/*.class \
+  jaunch.tar.gz
