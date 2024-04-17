842c739f0a9015d567f34c6891ec33949b10d26e
Curtis Rueden <ctrueden@wisc.edu>
2024-04-17 09:39:01 +0200

    Move JaunchOption data class to main.kt

--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -1,6 +1,20 @@
 import platform.posix.exit
 import kotlin.math.min
 
+@Suppress("ArrayInDataClass")
+data class JaunchOption(
+    val flags: Array<String>,
+    val assignment: String?,
+    val help: String?,
+) {
+    fun help(): String = buildString {
+        val indent = "$NL                    "
+        append(flags.joinToString(", "))
+        assignment?.let { append(" $it") }
+        help?.let { append("$indent${it.replace("\n", indent)}") }
+    }
+}
+
 typealias JaunchOptions = Map<String, JaunchOption>
 
 fun main(args: Array<String>) {
