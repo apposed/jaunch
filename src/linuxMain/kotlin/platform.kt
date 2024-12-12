a15c48795d5d0973600befddfe78855d145bc0c3
Curtis Rueden <ctrueden@wisc.edu>
2024-12-12 11:01:51 -0600

    Add comments about why there are two mkdir impls

--- src/linuxMain/kotlin/platform.kt
+++ src/linuxMain/kotlin/platform.kt
@@ -4,6 +4,11 @@ import platform.posix.*
 
 @OptIn(ExperimentalForeignApi::class)
 actual fun mkdir(path: String): Boolean {
+    // NB: This function is here,
+    // rather than in posixMain/platform.kt,
+    // because Linux's mkdir wants a UInt,
+    // whereas macOS's mkdir wants a UShort.
+    // The implementations are otherwise identical.
     val result = mkdir(path, S_IRWXU.toUInt())
     if (result != 0) {
         val errorCode = errno
--- src/macosMain/kotlin/platform.kt
+++ src/macosMain/kotlin/platform.kt
@@ -4,6 +4,11 @@ import platform.posix.*
 
 @OptIn(ExperimentalForeignApi::class)
 actual fun mkdir(path: String): Boolean {
+    // NB: This function is here,
+    // rather than in posixMain/platform.kt,
+    // because macOS's mkdir wants a UShort,
+    // whereas Linux's mkdir wants a UInt.
+    // The implementations are otherwise identical.
     val result = mkdir(path, S_IRWXU.toUShort())
     if (result != 0) {
         val errorCode = errno
