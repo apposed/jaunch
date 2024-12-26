06429ae7b540e68095e403929b3c93839bd6e91d
Curtis Rueden <ctrueden@wisc.edu>
2024-12-26 16:07:12 -0600

    Use llvm-mingw to cross-compile Windows targets
    Thanks to Martin Storsjö for this awesome project!

--- bin/compile-launcher.sh
+++ bin/compile-launcher.sh
@@ -17,6 +17,29 @@ test -d "$jdkdir" || {
 
 mkdir -p build
 
+find-llvm-mingw() {
+  find .cache/llvm-mingw -maxdepth 1 -type d 2>/dev/null | sort -r | head -n1
+}
+
+configure-llvm-mingw() {
+  cdir=$(find-llvm-mingw)
+  if [ "$cdir" ]
+  then
+    echo "$cdir"
+  else
+    v=20241217
+    n=llvm-mingw-$v-msvcrt-ubuntu-20.04-x86_64
+    mkdir -p .cache/llvm-mingw &&
+    (
+      cd .cache/llvm-mingw &&
+      set -x &&
+      curl -fLO https://github.com/mstorsjo/llvm-mingw/releases/download/$v/$n.tar.xz &&
+      tar xf $n.tar.xz
+    ) &&
+    echo "$(find-llvm-mingw)"
+  fi
+}
+
 compile() {
   compiler=$1
   command -v "$compiler" >/dev/null 2>&1 || {
@@ -36,17 +59,42 @@ compile() {
 
 case "$(uname -s)" in
   Linux)
+    # Compile Linux targets.
+    compile aarch64-linux-gnu-gcc -o build/launcher-linux-arm64 &&
     compile gcc -o build/launcher-linux-x64 &&
-    compile aarch64-linux-gnu-gcc -o build/launcher-linux-arm64
+    # Cross-compile Windows targets (thanks, llvm-mingw!).
+    cdir=$(configure-llvm-mingw)
+    if [ -d "$cdir" ]
+    then
+      # windows-arm64 (console and GUI builds)
+      compile "$cdir/bin/aarch64-w64-mingw32-clang" \
+        -o build/launcher-windows-arm64.exe -mconsole &&
+      compile "$cdir/bin/aarch64-w64-mingw32-clang" \
+        -o build/launcher-windows-arm64.exe -mwindows &&
+      # windows-x64 (console and GUI builds)
+      compile "$cdir/bin/x86_64-w64-mingw32-clang" \
+        -o build/launcher-windows-x64.exe -mconsole &&
+      compile "$cdir/bin/x86_64-w64-mingw32-clang" \
+        -o build/launcher-windows-x64.exe -mwindows
+    else
+      echo "[WARNING] Failed to set up llvm-mingw; skipping Windows cross-compilation"
+    fi
     ;;
   Darwin)
+    # Compile macOS targets.
     compile gcc -o build/launcher-macos-arm64 -framework CoreFoundation -target arm64-apple-macos11 &&
     compile gcc -o build/launcher-macos-x64 -framework CoreFoundation -target x86_64-apple-macos10.12 &&
     (set -x; lipo -create -output build/launcher build/launcher-macos-x64 build/launcher-macos-arm64)
     ;;
   MINGW*|MSYS*)
-    compile gcc -o build/launcher-windows-x64-console.exe -mconsole
-    compile gcc -o build/launcher-windows-x64-gui.exe -mwindows
+    # Compile Windows targets (current arch only).
+    arch=$(uname -m)
+    if [ "$arch" = "x86_64" ]; then arch=x64; fi
+    compile gcc -o "build/launcher-windows-$arch-console.exe" -mconsole
+    compile gcc -o "build/launcher-windows-$arch-gui.exe" -mwindows
     ;;
-  *) compile gcc -o build/launcher ;;
+  *)
+    # Unknown architecture; just try it.
+    compile gcc -o build/launcher
+    ;;
 esac
