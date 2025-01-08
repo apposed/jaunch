9cfd02241a139139421158758bc7aebbc126fcf9
Curtis Rueden <ctrueden@wisc.edu>
2025-01-08 14:16:13 -0600

    Tweak launcher batch file preamble

--- configs/launcher.bat
+++ configs/launcher.bat
@@ -3,8 +3,8 @@
 rem # This batch file is a shortcut for launching the application
 rem # without regard for the underlying architecture (x86-64 or arm64).
 rem # It also forces a new console to be allocated if the parent process
-rem # does not already have one, whichis especially useful for executables
-rem # compiled in GUI mode rather than console mode, so that:
+rem # does not already have one, which is especially useful for
+rem # executables compiled in GUI mode rather than console mode, so that:
 rem #
 rem # 1) the shell prompt blocks until the program completes; and
 rem #
@@ -16,7 +16,7 @@ rem # can be launched in blocking mode from an existing PowerShell via:
 rem #
 rem #     Start-Process -Wait .\launcher-windows-x64.exe
 rem #
-rem # Or from an existing Command Prompt via:
+rem # or from an existing Command Prompt via:
 rem #
 rem #     start /wait launcher-windows-x64.exe
 rem #
