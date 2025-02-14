d6499e2949391f363882b9afe5ecdfbd28efd78c
Curtis Rueden <ctrueden@wisc.edu>
2025-02-14 16:29:38 -0600

    Use appification script calls to build the demos

--- bin/demo.sh
+++ bin/demo.sh
@@ -6,109 +6,6 @@ echo -e "\033[1;33m[demo]\033[0m"
 
 test -d dist || { echo '[ERROR] No dist folder; please `make dist` first.' 1>&2; exit 1; }
 
-demoDir=demo
-
-targetDir() {
-  if [ "$1" ]; then result="$demoDir/$1"; else result="$demoDir"; fi
-  if [ ! -d "$result" ]; then (set -x; mkdir -p "$result"); fi
-  echo "$result"
-}
-
-if [ ! -d "$demoDir/jaunch" ]; then (set -x; mkdir -p "$demoDir"/jaunch); fi
-
-# Copy launcher binaries within the dist folder, duplicating to all demo apps.
-#
-# We use the dist folder rather than build because those binaries may have
-# been packed by upx, whereas those in build are the raw compiled outputs.
-#
-# And we use this awkward find construction to capture launcher binaries in
-# both the base dist folder as well as nested under dist/Contents/MacOS.
-echo 'Copying launcher binaries...'
-find dist -name 'launcher-*' | while read f
-do
-  srcDir=${f%/launcher*}
-  targetDir=$(targetDir "${srcDir#dist}")
-  suffix=${f#*/launcher}
-
-  # Strip Windows-specific -console and -gui suffixes.
-  case "$suffix" in
-    *-console.exe) suffix="${suffix%-console.exe}.exe" ;;
-    *-gui.exe) suffix="${suffix%-gui.exe}.exe" ;;
-  esac
-
-  # Populate console apps.
-  test "$f" != "${f%-gui.exe}" && winGUI=true || winGUI=false
-  if [ "$winGUI" = false ]
-  then
-    for app in hi jy parsy paunch repl
-    do
-      (set -x; cp "$f" "$targetDir/$app$suffix")
-    done
-  fi
-
-  # Populate GUI apps.
-  test "$f" != "${f%-console.exe}" && winConsole=true || winConsole=false
-  if [ "$winConsole" = false ]
-  then
-    for app in hello
-    do
-      (set -x; cp "$f" "$targetDir/$app$suffix")
-    done
-  fi
-done
-
-# Copy wrapper launch scripts.
-for app in jy parsy paunch repl hello hi
-do
-  case "$(uname -s)" in
-    MINGW*|MSYS*) (
-      set -x
-      cp configs/launcher.bat "$demoDir/$app.bat"
-    );;
-    *) (
-      set -x
-      cp configs/launcher.sh "$demoDir/$app"
-      chmod +x "$demoDir/$app"
-    );;
-  esac
-done
-
-# Copy simple Java program classes.
-for prog in HelloSwing HelloWorld
-do
-  (set -x; cp "configs/$prog.class" "$demoDir/")
-done
-
-# Copy jaunch configurator binaries.
-echo 'Copying jaunch configurator binaries...'
-find dist -name 'jaunch-*' | while read f
-do
-  srcDir=${f%/jaunch-*}
-  targetDir=$(targetDir "${srcDir#dist/}")
-  (set -x; cp "$f" "$targetDir/")
-done
-
-# Copy Props.class helper program and TOML configuration files.
-echo 'Copying configuration files and helpers...'
-for f in \
-  Props.class \
-  common.toml \
-  hello.toml \
-  hi.toml \
-  jvm.toml \
-  jy.toml \
-  parsy.toml \
-  paunch.toml \
-  python.toml \
-  repl.toml
-do
-  if [ ! -f "$demoDir/jaunch/$f" ]
-  then
-    (set -x; cp "configs/$f" "$demoDir/jaunch/")
-  fi
-done
-
-# Install needed JAR files.
 copyDependency() {
   g=$1
   a=$2
@@ -123,8 +20,66 @@ copyDependency() {
   then
     mkdir -p "$demoDir/lib"
     echo "Copying $a..."
-    (set -x; cp .cache/lib/$a-$v.jar "$demoDir/lib/")
+    cp -v .cache/lib/$a-$v.jar "$demoDir/lib/"
   fi
 }
-copyDependency org/scijava parsington 3.1.0 # dependency of parsy
-copyDependency org/python jython 2.7.4      # dependency of jy
+
+selectWindowsEXE() {
+  exe=$1
+  kind=$2
+  for f in "$demoDir/$exe-windows-"*; do
+    case "$f" in
+      *-$kind.exe) mv -v "$f" "${f%-$kind.exe}.exe" ;;
+      *) rm -v "$f" ;;
+    esac
+  done
+}
+
+demoDir=demo
+mkdir -p "$demoDir"
+
+# Hi
+bin/appify.sh --out-dir "$demoDir" --app-title Hi --app-exe hi \
+  --app-id org.apposed.jaunch.hi --jaunch-toml configs/hi.toml \
+  --app-icon icons/1F44B-waving-hand.svg
+cp -v configs/HelloWorld.* "$demoDir"
+selectWindowsEXE hi console
+
+# Hello
+bin/appify.sh --out-dir "$demoDir" --app-title Hello --app-exe hello \
+  --app-id org.apposed.jaunch.hello --jaunch-toml configs/hello.toml \
+  --app-icon icons/E1C0-wireframes.svg
+cp -v configs/HelloSwing.* "$demoDir"
+selectWindowsEXE hello gui
+
+# Jy
+bin/appify.sh --out-dir "$demoDir" --app-title Jy --app-exe jy \
+  --app-id org.apposed.jaunch.jy --jaunch-toml configs/jy.toml \
+  --app-icon icons/2615-hot-beverage.svg
+copyDependency org/python jython 2.7.4
+selectWindowsEXE jy console
+
+# Parsy
+bin/appify.sh --out-dir "$demoDir" --app-title Parsy --app-exe parsy \
+  --app-id org.apposed.jaunch.parsy --jaunch-toml configs/parsy.toml \
+  --app-icon icons/1F523-input-symbols.svg
+copyDependency org/scijava parsington 3.1.0
+selectWindowsEXE parsy console
+
+# Paunch
+bin/appify.sh --out-dir "$demoDir" --app-title Paunch --app-exe paunch \
+  --app-id org.apposed.jaunch.paunch --jaunch-toml configs/paunch.toml \
+  --app-icon icons/1F40D-snake.svg
+# Note: We leave both EXE files (console and gui) for Paunch.
+
+# REPL
+bin/appify.sh --out-dir "$demoDir" --app-title REPL --app-exe repl \
+  --app-id org.apposed.jaunch.repl --jaunch-toml configs/repl.toml \
+  --app-icon icons/E1C1-code-editor.svg
+selectWindowsEXE repl console
+
+# Remove .sh extension from shell scripts.
+# Just so it's a little bit easier to launch!
+for f in "$demoDir"/*.sh; do
+  mv "$f" "${f%.sh}"
+done
