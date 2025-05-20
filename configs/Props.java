832dc164e18d9121712f4942b0743e3c44040b81
hinerm <hinerm@gmail.com>
2025-05-20 09:39:16 -0500

    Update Props with optional file param
    Allows for Props to write its output to a File instead of system.out

--- configs/Props.java
+++ configs/Props.java
@@ -1,11 +1,31 @@
 import java.util.*;
+import java.io.*;
+
 public class Props {
   public static void main(String[] args) {
-    Properties props = System.getProperties();
-    for (Enumeration e = props.propertyNames(); e.hasMoreElements();) {
-      String key = e.nextElement().toString();
-      String value = props.getProperty(key);
-      System.out.println(key + "=" + value);
+    PrintWriter out = null;
+    final boolean printToFile = args.length > 0;
+
+    try {
+      if (printToFile) {
+        out = new PrintWriter(new BufferedWriter(new FileWriter(args[0])));
+      } else {
+        out = new PrintWriter(System.out, true);
+      }
+
+      Properties props = System.getProperties();
+      for (Enumeration<?> e = props.propertyNames(); e.hasMoreElements(); ) {
+        String key = e.nextElement().toString();
+        String value = props.getProperty(key);
+        out.println(key + "=" + value);
+      }
+
+    } catch (IOException e) {
+      e.printStackTrace();
+    } finally {
+      if (out != null && printToFile) {
+        out.close();
+      }
     }
   }
 }
