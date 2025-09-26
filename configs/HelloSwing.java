80ef7cdbfd178eb99b63dbce0a3ee0b42a534e79
Curtis Rueden <ctrueden@wisc.edu>
2025-09-26 14:05:20 -0500

    Remove dead code from HelloSwing demo app

--- configs/HelloSwing.java
+++ configs/HelloSwing.java
@@ -1,12 +1,6 @@
 import javax.swing.*;
 
 public class HelloSwing {
-  public static String propRow(String name) {
-    return "<tr>" +
-      "<td>" + name + "</td>" +
-      "<td>" + System.getProperty(name) + "</td>" +
-      "</tr>";
-  }
   public static void main(String[] args) {
     JOptionPane.showMessageDialog(null,
       "<html>" +
